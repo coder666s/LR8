b79df87eab39a8b23b6e74e92838d1da7ae8b5e5
coder666s <91024517+coder666s@users.noreply.github.com>
2022-10-12 20:17:19 +0300

    1
    1

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import fileFilter from "./fileFilter.js";
+
+const app = express(); 
+
+const PUBLIC_DIR = "public"; 
+app.use(express.static(PUBLIC_DIR));
+
+app.post("/create", fileFilter, (req, res) => { 
+  if (!req.body?.name || !req.files[0]?.path) {
+    let error = {
+      status: "error",
+      message: "Не хватает данных",
+    };
+    res.statusCode = 400;
+    res.send(error);
+    return;
+  }
+
+  let model = {
+    name: req.body.name, 
+    description: req.body.description,
+    image: req.files[0].path
+  };
+
+  let answer = {
+    status: "ok",
+    data: model,
+  };
+  res.statusCode = 200;
+  res.send(answer);
+});
+
+const port = process.env.PORT || 3000; 
+app.listen(port, () => console.log(`Server started at port http://localhost:3000/`));
